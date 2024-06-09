766218554df595ec17af373647ac5bfe50b3b2a9
minivan <7175380@qq.com>
2024-06-09 13:43:12 +0800

    fix: fix some error

--- scripts/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const db = mysql.createPool({
-  host: 'localhost',   
-  user: 'root',        
-  database: 'timeagent',    
-});
-
-module.exports = { db };
